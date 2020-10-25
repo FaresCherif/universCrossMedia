53114b98f4a8806ad57de49bf7e177c5518e9602
Cherif Fares <fares.cherif@etu.unilim.fr>
2020-10-25 12:33:55 +0100

    simplification nom page

--- include/texte.inc.php
+++ include/texte.inc.php
@@ -8,23 +8,27 @@ else{
 
 switch ($page) {
 	case 0:
-		include_once('pages/pageAccueil.inc.php');
+		include_once('pages/Accueil.inc.php');
 	break;
 
 	case 1:
-		include_once('pages/pageAccueilFilm.inc.php');
+		include_once('pages/AccueilFilm.inc.php');
 	break;
 
 	case 2:
-		include_once('pages/pageAccueilJeuVideo.inc.php');
+		include_once('pages/AccueilJeuVideo.inc.php');
 	break;
 
 	case 3:
-		include_once('pages/pageGenreFilm.inc.php');
+		include_once('pages/GenreFilm.inc.php');
 		break;
 
 	case 4:
-		include_once('pages/pageUniversFilm.inc.php');
+		include_once('pages/UniversFilm.inc.php');
+		break;
+
+	case 5:
+		include_once('pages/Film.inc.php');
 		break;
 
 
