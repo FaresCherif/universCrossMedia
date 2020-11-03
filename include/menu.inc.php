7e87d6354e9e95946f0f152f847f307594993a00
Cherif Fares <fares.cherif@etu.unilim.fr>
2020-11-03 17:26:45 +0100

    ajout menu

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -1,4 +1,8 @@
 <?php
-if (utilisateurEstConnecte()){
-
+if (utilisateurEstConnecte() && utilisateur()->getPermission()==2){
+  ?>
+  <ul>
+  	<li><a href="index.php?page=13">Lister les utilisateurs</a></li>
+  </ul>
+  <?php
 } ?>
